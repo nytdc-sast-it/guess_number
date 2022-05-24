e192f19430c3147bf77d6429385f8e9711b764e8
Taoning Ge <gtn1024o@outlook.com>
2022-05-24 22:24:52 +0800

    获取用户猜测的数字

--- src/main/java/org/tdsast/GuessNumber.java
+++ src/main/java/org/tdsast/GuessNumber.java
@@ -1,7 +1,16 @@
 package org.tdsast;
 
+import java.util.Scanner;
+
 public class GuessNumber {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===== 猜数字游戏 =====");
+        System.out.print("请输入你猜的数字：");
+        int inputNum = scanner.nextInt();
+        System.out.println("你刚才输入的数字是 " + inputNum);
+
+        scanner.close();
     }
 }
