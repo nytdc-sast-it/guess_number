0e78ca7e325b9654da3f2ee517e984f905da84c5
Taoning Ge <gtn1024o@outlook.com>
2022-05-25 10:40:16 +0800

    第二部分猜数字代码

--- src/main/java/org/tdsast/GuessNumber.java
+++ src/main/java/org/tdsast/GuessNumber.java
@@ -1,15 +1,28 @@
 package org.tdsast;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Random random = new Random();
+        int number = random.nextInt(100);
+        System.out.println("生成的数字是：" + number);
+
         System.out.println("===== 猜数字游戏 =====");
         System.out.print("请输入你猜的数字：");
         int inputNum = scanner.nextInt();
         System.out.println("你刚才输入的数字是 " + inputNum);
+
+        if (inputNum > number) {
+            System.out.println("大了");
+        } else if (inputNum < number) {
+            System.out.println("小了");
+        } else {
+            System.out.println("猜对了");
+        }
 
         scanner.close();
     }
